c2448cdbb2cae5a003d41d2c418e892e98c7152d
kvin1087 <kaustubh87@gmail.com>
2017-07-10 15:20:11 -0500

    Hiding some api methods

--- common/models/review.js
+++ common/models/review.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = function(Review) {
+  //Disable endpoint methods
+  Review.disableRemoteMethod("count", true);
+  Review.disableRemotemethod("exists", true);
+  Review.disableRemoteMethod("findOne", true);
+  Review.disableRemoteMethod('createChangeStream', true);
+  Review.disableRemoteMethod("updateAll", true);
 
 };
